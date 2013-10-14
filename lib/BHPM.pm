d0cf12d10178ce7c02fb04787516fd9107ff5f2c
lucas1 <lucastiagodemoraes@gmail.com>
2013-10-13 22:27:58 -0300

    Update BHPM.pm

--- lib/BHPM.pm
+++ lib/BHPM.pm
@@ -1,11 +1,20 @@
 package BHPM;
 use Dancer2;
+use WWW::Mechanize;
 use Lista;
 
 prefix undef;
 
 get '/' => sub {
-    template 'index';
+    my $mech = WWW::Mechanize->new;
+    
+    $mech->get('');
+    
+    my $content = $mech->content;
+    
+    $content =~ m{Perl News<\/a><\/li>(.*?)<\/ul>}s;
+    
+    template 'index', {news => $1};
 };
 
 get '/sobre' => sub {
