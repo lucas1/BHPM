968fecbfd5abf649c4abc608cd77b1ed95e7805e
lucas1 <lucastiagodemoraes@gmail.com>
2013-10-13 22:51:20 -0300

    Update BHPM.pm

--- lib/BHPM.pm
+++ lib/BHPM.pm
@@ -14,7 +14,7 @@ get '/' => sub {
     
     $content =~ m{CPAN<\/a><\/li>(.*?)<\/ul>}s;
     my $update = $1;
-    $update =~ s/http:\/\/search\.cpan\.org\/~[a-z0-9]{1,}\//https:\/\/metacpan\.org\/module\//gs;
+    $update =~ s/http:\/\/search\.cpan\.org\/~?[a-z0-9]{1,}\//https:\/\/metacpan\.org\/module\//gs;
     $update =~ s/-[0-9]{1,}\.[0-9]{1,}//gs;
     
     $content =~ m{Perl News<\/a><\/li>(.*?)<\/ul>}s;
